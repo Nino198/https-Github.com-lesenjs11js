45adbcae406c34de19510ca81f2718c2c47c1e2e
Nino198 <151477098+Nino198@users.noreply.github.com>
2024-01-16 07:16:07 +0400

    Initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+<link rel="stylesheet" hrf="reset.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+  <a hrf'Inbox.html'> HOME </a> |
+  <a hrf'about.html'> about</a> |
+  <a hrf'Gallery.html'> Gallery </ahrf> |
+  <a hrf'contact.html'> contact </a>  
+<h1></h1>
+</body>
+</html>
